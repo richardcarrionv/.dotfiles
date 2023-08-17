b7f3a50c9cd83f5798fb2d33a535bd72e91c62c1
Richard Carrion <noesrichard@hotmail.com>
2023-08-17 02:54:15 -0500

    updates

--- bin/.local/scripts/market-startup.sh
+++ bin/.local/scripts/market-startup.sh
@@ -5,7 +5,7 @@ market_api="/home/carri/workspace/market-api/"
 market="/home/carri/workspace/market/"
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s "market-api" -c $market_api -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window \; new-session -s "market" -c $market -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market -n "vite" "zsh -c 'yarn dev; zsh -i'" \; previous-window
+  tmux new-session -s "market-api" -c $market_api -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window \; new-session -s "market" -c $market -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market -n "vite" "zsh -c 'yarn dev; zsh -i'" \; previous-window \; new-session -s "home" -c "/home/carri/" -n "zsh"
   exit 0
 fi
 

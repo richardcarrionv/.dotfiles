9d516f750a37621332e8d884d4dee2a2c38538d1
Richard Carrion <noesrichard@hotmail.com>
2023-08-18 05:49:51 -0500

    Updates

--- .config/i3/start-devenv.sh
+++ .config/i3/start-devenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-i3-msg 'workspace 3; exec alacritty -e sh -C /home/carri/bin/.local/scripts/market-startup.sh'
+i3-msg 'workspace 4; exec alacritty -e sh -C /home/carri/bin/.local/scripts/market-startup.sh'
 sleep 0.5
 i3-msg 'workspace 5; exec google-chrome --new-window https://linear.app/noesrichard/team/PU/cycle/active'
 sleep 3
 i3-msg 'workspace 7; exec google-chrome --new-window http://localhost:3000 http://localhost/api/v1/console/docs'
 sleep 1
-i3-msg 'workspace 3'
+i3-msg 'workspace 4'

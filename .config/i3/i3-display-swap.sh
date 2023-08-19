c035dca7193eabefa74a1babbfddb30d8b407084
Richard Carrion <noesrichard@hotmail.com>
2023-08-19 18:38:38 -0500

    Updates

--- .config/i3/i3-display-swap.sh
+++ .config/i3/i3-display-swap.sh
@@ -5,8 +5,8 @@
 stick_to="output" && cmd="focus output" 	# stick to the screen
 
 initial=$(
-	i3-msg -t get_workspaces |
-	jq -r ".[] | select(.focused == true) | .$stick_to"
+    i3-msg -t get_workspaces |
+    jq -r ".[] | select(.focused == true) | .$stick_to"
 )
 
 py_script=$(cat <<EOF
@@ -27,12 +27,12 @@ jq -r '.[] | select(.active == true) | "\(.name):\(.current_workspace)"' |
 python3 -c "$py_script" |
 while read -r screen_src screen_dst workspace_tgt workspace_final
 do
-	i3-msg -- workspace --no-auto-back-and-forth "$workspace_tgt"
-	i3-msg move workspace to output "$screen_dst"
-	i3-msg focus output "$screen_src"
-	i3-msg -- workspace --no-auto-back-and-forth "$workspace_final"
-	echo "moved $workspace_tgt to $screen_dst"
-	echo "and restored focus to $workspace_final"
+    i3-msg -- workspace --no-auto-back-and-forth "$workspace_tgt"
+    i3-msg move workspace to output "$screen_dst"
+    i3-msg focus output "$screen_src"
+    i3-msg -- workspace --no-auto-back-and-forth "$workspace_final"
+    echo "moved $workspace_tgt to $screen_dst"
+    echo "and restored focus to $workspace_final"
 done
 
 i3-msg $cmd $initial
--- bin/.local/scripts/market-startup.sh
+++ bin/.local/scripts/market-startup.sh
@@ -5,12 +5,12 @@ market_api="/home/carri/workspace/market-api/"
 market="/home/carri/workspace/market/"
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s "market-api" -c $market_api -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window \; new-session -s "market" -c $market -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market -n "vite" "zsh -c 'yarn dev; zsh -i'" \; previous-window \; new-session -s "home" -c "/home/carri/" -n "zsh"
+  tmux new-session -s "market-api" -c $market_api -n 'neovim' "zsh -c 'source ./venv/bin/activate;nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window \; new-session -s "market" -c $market -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market -n "vite" "zsh -c 'yarn dev; zsh -i'" \; previous-window \; new-session -s "home" -c "/home/carri/" -n "zsh"
   exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-  tmux new-session -ds "market-api" -c $market_api -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window
+  tmux new-session -ds "market-api" -c $market_api -n 'neovim' "zsh -c 'source ./venv/bin/activate; nvim .; zsh -i'" \; new-window -c $market_api -n "docker" "zsh -c 'docker compose up; zsh -i'" \; previous-window
   tmux new-session -ds "market" -c $market -n 'neovim' "zsh -c 'nvim .; zsh -i'" \; new-window -c $market -n "vite" "zsh -c 'yarn dev; zsh -i'" \; previous-window
 fi
 

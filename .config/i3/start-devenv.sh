38f87b5e8b210be156acca86ee424f5cfa183dcb
Richard Carrion <noesrichard@hotmail.com>
2024-01-22 22:49:31 -0500

    updates

--- .config/i3/start-devenv.sh
+++ .config/i3/start-devenv.sh
@@ -7,6 +7,6 @@ i3-msg 'workspace 4; exec alacritty -e tmux attach -t api'
 sleep 1
 i3-msg 'workspace 5; exec google-chrome --new-window https://linear.app/noesrichard/team/PU/projects/all'
 sleep 3
-i3-msg 'workspace 7; exec google-chrome --new-window http://localhost:3000 http://localhost:3000/dashboard http://localhost/api/v1/docs'
+i3-msg 'workspace 7; exec google-chrome --new-window http://localhost:3000 http://localhost/api/v1/docs; exec brave-browser http://localhost:3000; exec firefox http://localhost:3000; layout stacked'
 sleep 1
 i3-msg 'workspace 4'

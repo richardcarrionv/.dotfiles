0ddd2fcb0995303c5b9cef08eb82a9b2cfe11d60
Richard Carrion <richardcarrionv@hotmail.com>
2024-05-25 16:01:30 -0500

    sxhkd

--- .github/README.md
+++ .github/README.md
@@ -2,7 +2,7 @@
 
 ## Dependencies
 ```
-alacritty tmux neovim polybar rofi i3 zsh fzf feh xrandr sxhkd xclip
+alacritty tmux neovim polybar rofi i3 zsh fzf feh xrandr sxhkd xclip qalc
 ```
 - Espanso
 
--- .local/bin/start-tesis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmuxp load --yes /home/carri/.config/tmux/tesis.yaml
-
-i3-msg 'workspace 4; exec mupdf-gl /home/carri/workspace/tesis/out/main.pdf; exec alacritty -e tmux attach -t tesis'
-sleep 1
-i3-msg 'resize grow width 20 px or 20 ppt'
